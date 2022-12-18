b53c0f2160d2b793e5d671919919931bf6de430d
naonao708 <naonao708@outlook.com>
2022-12-18 07:19:21 +0000

    Chap "関数の引数"

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() { 
-    another_function(5);
+    print_labeled_mesurement(5, 'h');
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {}", x);
+fn print_labeled_mesurement(value: i32, unit_label: char) {
+    println!("The mesurement is: {}{}", value, unit_label);
 }
